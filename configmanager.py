51dd9110e4d630d22982f98bafed0821792c805b
Archanciel <jp.schnyder@gmail.com>
2020-06-20 21:04:57 +0200

    added dropbox dir parm

--- configmanager.py
+++ configmanager.py
@@ -6,6 +6,7 @@ CONFIG_SECTION_PROJECTS = 'Projects'
 
 CONFIG_KEY_DOWNLOAD_PATH = 'downnloadPath'
 CONFIG_KEY_DROPBOX_API_KEY = 'dropboxApiKey'
+CONFIG_KEY_DROPBOX_BASE_DIR = 'dropboxBaseDir'
 
 #CONFIG_KEY_TRANS_FILE_CLOUD_TEST_PROJECT = 'transFileCloudTestProjectPath'
 CONFIG_KEY_PROJECT_PATH = 'projectPath'
@@ -29,6 +30,11 @@ class ConfigManager:
 			raise KeyError('\'' + e.args[0] + '\' parameter not defined in ' + configFilePathName)
 
 		try:
+			self.dropboxBaseDir = self.config[CONFIG_SECTION_GENERAL][CONFIG_KEY_DROPBOX_BASE_DIR]
+		except KeyError as e:
+			raise KeyError('\'' + e.args[0] + '\' parameter not defined in ' + configFilePathName)
+
+		try:
 			self.projects = self.config[CONFIG_SECTION_PROJECTS]
 		except KeyError as e:
 			raise KeyError('\'' + e.args[0] + '\' section not defined in ' + configFilePathName)
@@ -40,10 +46,9 @@ if __name__ == '__main__':
 		configFilePathName = 'c:\\temp\\transfiles.ini'
 
 	cm = ConfigManager(configFilePathName)
-	#print(cm.downloadPath)
-	#print(cm.dropboxApiKey)
-
-	#print(cm.config[CONFIG_SECTION_PROJECTS]['transFileCloudTestProject'])
+	print(cm.dropboxBaseDir)
+	for key in cm.config['General']: 
+		print(key)
 	
 	for key in cm.projects: 
 		print(key)
