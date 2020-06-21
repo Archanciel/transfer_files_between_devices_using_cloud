a089f32590b547c620f66d772b63bbccb02a678b
Archanciel <jp.schnyder@gmail.com>
2020-06-21 18:37:32 +0200

    init commit

---
+++ dropbox_create_folder.py
@@ -0,0 +1,30 @@
+import os, io, dropbox
+
+from configmanager import ConfigManager
+
+if os.name == 'posix':
+	CONFIG_FILE_PATH_NAME = '/sdcard/transfiles.ini'
+else:
+	CONFIG_FILE_PATH_NAME = 'c:\\temp\\transfiles.ini'
+	
+def createEmptyFolder(dropboxBaseFolder, newFolder):
+	# creating a temp dummy destination file path
+	dummyFileTo = dropboxBaseFolder + newFolder + '/' + 'temp.bin'
+
+	# creating a virtual in-memory binary file
+	f = io.BytesIO(b"\x00")
+
+	# uploading the dummy file in order to create the containing folder		
+	dbx.files_upload(f.read(), dummyFileTo)
+	
+	# now that the folder is created, delete the dummy file	
+	dbx.files_delete(dummyFileTo)
+
+cm = ConfigManager(CONFIG_FILE_PATH_NAME)
+access_token = cm.dropboxApiKey
+dbx = dropbox.Dropbox(access_token)
+
+dropboxBaseDir = '/test_dropbox'
+dropboxNewSubDir = '/new_empty_sub_dir'
+
+createEmptyFolder(dropboxBaseDir, dropboxNewSubDir)
