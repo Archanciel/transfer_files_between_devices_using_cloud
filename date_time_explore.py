ab0481b959d4ce4ff0653e8ae863b76ddc4d956b
Archanciel <jp.schnyder@gmail.com>
2020-06-22 16:59:46 +0200

    Update date_time_explore.py

--- date_time_explore.py
+++ date_time_explore.py
@@ -1,10 +1,11 @@
-import datetime
+from datetime import datetime
 
 format = '%Y-%m-%d %H:%M:%S'
 
-dateMod = datetime.datetime.strptime('2020-06-14 08:45:23', format)
+dateMod = datetime.strptime('2020-06-14 08:45:23', format)
 print(dateMod)
 
 format2 = '%y-%m-%d %H:%M:%S'
 
 print(dateMod.strftime(format2))
+print(datetime.now().strftime(format))
