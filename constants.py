160e6f25f0432bf5a135fd36e3a174d13231f9b9
Archanciel <jp.schnyder@gmail.com>
2020-07-12 17:04:59 +0200

    removed no longer used csts

--- constants.py
+++ constants.py
@@ -6,15 +6,7 @@ DATE_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 if os.name == 'posix':
 	DIR_SEP = '/'
 	CONFIG_FILE_PATH_NAME = '/sdcard/transfiles.ini'
-	BASE_DST_FILE_DIR = '/storage/emulated/0/Android/data/ru.iiec.pydroid3/files/' # to remove
-	TEST_SUB_DIR = '/test'
-	IMG_SUB_DIR = '/images'
-	DOC_SUB_DIR = '/doc'	
 else:
 	# Windows
 	DIR_SEP = '\\'
 	CONFIG_FILE_PATH_NAME = 'c:\\temp\\transfiles.ini'
-	BASE_DST_FILE_DIR = 'D:\\Development\\Python' # to remove
-	TEST_SUB_DIR = '\\test'
-	IMG_SUB_DIR = '\\images'
-	DOC_SUB_DIR = '\\doc'
